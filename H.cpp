042d92a5a9108045f1a4fa80a517182a28cd4eab
ramisa-zahara <ramisazahara@gmail.com>
2023-02-06 14:23:30 +0600

    problem H

---
+++ H.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    for(int i=0;i<=5000;i++){
+        for(int j=0;j<=5000;j++){
+            if((a*i)+(b*j)==c){
+                cout<<"Yes\n";
+                return 0;
+            }
+           
+        }
+    }
+     cout<<"No\n";
+    return 0;
+}
